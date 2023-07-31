836687c5230c90b7b39d8d646e92627cd55a3c42
αηdr3α5 ɮαηɦօlʐ3ʀ <AndreasBanholzer@Me.com>
2023-07-31 23:07:04 +0200

    add methods

--- __generate/0-generate-environment/src/Generate.mjs
+++ __generate/0-generate-environment/src/Generate.mjs
@@ -180,7 +180,7 @@ export class GenerateOptions {
         const category = [ this.#titleizeString( struct['category'] ), struct['category'] ]
         console.log( 'item', item )
         const methods = item['methods']
-            .map( method => `[${method}](../${this.#config['url']['methods']}/${method}.html#options)` )
+            .map( method => `[${method}](../${this.#config['url']['methods']}${method}.html#options)` )
             .join( ', ' )
 
         struct['data']['key_row'] = [
