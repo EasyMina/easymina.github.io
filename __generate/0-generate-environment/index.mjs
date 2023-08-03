8c945648437ae3526708b7bb98dc621ea07338ca
αηdr3α5 ɮαηɦօlʐ3ʀ <AndreasBanholzer@Me.com>
2023-08-03 02:12:07 +0200

    insert introduction video

--- __generate/0-generate-environment/index.mjs
+++ __generate/0-generate-environment/index.mjs
@@ -1,10 +1,15 @@
 import fs from 'fs'
 import { GenerateOptions } from './src/Generate.mjs'
+import { EasyMina } from 'easymina'
 
+/*
+    const config = './data/0-config.json'
+    const raw = fs.readFileSync( config, 'utf-8' )
+    const easyMinaConfig = JSON.parse( raw )
+*/
 
-const config = './data/0-config.json'
-const raw = fs.readFileSync( config, 'utf-8' )
-const easyMinaConfig = JSON.parse( raw )
+const easyMina = new EasyMina()
+const easyMinaConfig = easyMina.getConfig()
 
 const generateOptions = new GenerateOptions()
 generateOptions.init( { 'config': easyMinaConfig } )
